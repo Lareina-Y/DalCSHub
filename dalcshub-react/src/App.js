96da15a14aaf617a38db971215d0cbd60ffa615e
Khaled Al-Mahbashi <kh958318@dal.ca>
2023-06-19 14:24:19 -0300

    added routing for faq page

--- dalcshub-react/src/App.js
+++ dalcshub-react/src/App.js
@@ -1,6 +1,7 @@
 import { Routes, Route, BrowserRouter } from "react-router-dom";
 import { MainFeed } from "./pages";
 import { NavBar } from "./components";
+import { FAQ } from "./pages/FAQ";
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
           <NavBar />
           <Routes>
             <Route exact path="/" element={<MainFeed />} />
+            <Route exact path="/faq" element={<FAQ />} />
           </Routes>
       </BrowserRouter> 
     </div>
