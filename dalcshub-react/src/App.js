491cb2524e1bd5e516bf46c3583c132f78e93517
Khaled Al-Mahbashi <kh958318@dal.ca>
2023-06-19 14:58:34 -0300

    App.js

--- dalcshub-react/src/App.js
+++ dalcshub-react/src/App.js
@@ -1,11 +1,8 @@
 import { Routes, Route, BrowserRouter } from "react-router-dom";
 import { MainFeed } from "./pages";
 import { NavBar } from "./components";
-<<<<<<< HEAD
 import { FAQ } from "./pages/FAQ";
-=======
 import { Contact } from "./pages/Contact";
->>>>>>> f5eacbd69e54c708dcdc995b6684c8fa90e67d51
 
 function App() {
   return (
@@ -14,11 +11,8 @@ function App() {
           <NavBar />
           <Routes>
             <Route exact path="/" element={<MainFeed />} />
-<<<<<<< HEAD
             <Route exact path="/faq" element={<FAQ />} />
-=======
             <Route exact path="/Contact" element={<Contact></Contact>} />
->>>>>>> f5eacbd69e54c708dcdc995b6684c8fa90e67d51
           </Routes>
       </BrowserRouter> 
     </div>
