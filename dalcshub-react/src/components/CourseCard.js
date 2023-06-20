8ff383a92cf7ee64b58c5dd385a32b9fe2e7a093
Kent Xern Chew <kn222220@dal.ca>
2023-06-20 19:09:30 -0300

    modified fonts to Verdana for cross platform support

--- dalcshub-react/src/components/CourseCard.js
+++ dalcshub-react/src/components/CourseCard.js
@@ -11,14 +11,14 @@ export const CourseCard = (props) => {
           gutterBottom
           variant="h5"
           component="div"
-          sx={{ fontFamily: "Helvetica Neue", fontWeight: "500" }}
+          sx={{ fontFamily: "Verdana, Geneva, sans-serif", fontWeight: "600" }}
         >
           {name}
         </Typography>
         <Typography
           variant="body2"
           color="text.secondary"
-          sx={{ fontFamily: "Helvetica Neue", fontWeight: "500" }}
+          sx={{ fontFamily: "Verdana, Geneva, sans-serif", fontWeight: "500" }}
         >
           {description}
         </Typography>
