0a8f2555c43cceebb070101d8c7cfa1800d70558
Khaled Al-Mahbashi <kh958318@dal.ca>
2023-07-25 16:43:08 -0300

    added post model and route

---
+++ dalcshub-api/api/routes/post.js
@@ -0,0 +1,55 @@
+// Author: Khaled Al-Mahbashi
+const express = require('express');
+const Post = require('../models/post')
+const router = express.Router();
+
+// get post by ID
+router.get('/post/:id', (req, res) => {
+    const id = req.params.id;
+
+    try {
+        Post.findById(id).then(response =>{
+            if(response == null){
+                res.status(404).send("This post does not exist")
+            } else {
+                res.status(200).send("Post information: " + response)
+            }
+        })
+    } catch (err) {
+        return res.status(500).json({message: 'Internal server error'})
+    }
+})
+
+// add post
+router.post('/add', async (req, res) => {
+    const body = req.body;
+
+    try {
+        if(Object.keys(body).length == 0){
+            res.status.apply(400).json({success: false, data: 'Incorrect request'})
+        }  
+    } catch (err) {
+        return res.status(500).json({message: 'Internal server error'})
+    }
+
+    let newPost = await new Post();
+    newPost.title = body.title;
+    newPost.author = body.author;
+    newPost.description = body.description;
+    newPost.date;
+
+    newPost.save()
+    .then(result =>{
+        res.status(200).send({
+            newPost: result,
+            success: true,
+            "message": "Post has been added"
+        })
+    }).catch(err =>{
+        if (err.code === 11000) { // MongoDB error code for duplicate key (Lareina)
+             res.status(409).json({ success: false, message: "Course already exists." });
+        }
+        res.status(400).send({message: "Post request cannot be submitted " + err});
+    })
+})
+module.exports = router;
